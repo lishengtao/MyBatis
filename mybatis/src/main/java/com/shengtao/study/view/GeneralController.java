6a258273d1408fd3a504f25e5a41d294cd015faf
lishengtao <shengtao0077@163.com>
2013-03-12 21:04:57 +0800

    Maven + Spring + Spring MVC + MyBatis

---
+++ mybatis/src/main/java/com/shengtao/study/view/GeneralController.java
@@ -0,0 +1,16 @@
+package com.shengtao.study.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GeneralController {
+
+	@RequestMapping(value="index.do")
+	public void index_jsp(Model model){
+		model.addAttribute("liming", "ÀèÃ÷ÄãºÃ");
+		System.out.println("index.jsp");
+	}
+}
+
